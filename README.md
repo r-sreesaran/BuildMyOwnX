392f9abc7c4158a1cc6ad08bc3cac27e2a84ad96
r-sreesaran <r.sreesaran@gmail.com>
2023-10-08 09:39:32 +0530

    added functionality to count the number of words in the text file.

--- README.md
+++
@@ -1,6 +0,0 @@
-# BuildMyOwnX
-This repository has implementations of my pet projects
-
-https://codingchallenges.fyi/challenges/challenge-wc
-
-    The first challenge is to build my own unix command line tool
--- WC/src/test/java/org/example/WordProcessorTest.java
+++ WC/src/test/java/org/example/WordProcessorTest.java
@@ -18,4 +18,26 @@ class WordProcessorTest {
         System.out.println(processor.getTotalSpace(processor.readFile(options[2])));
 
     }
+
+    @Test
+    void testTotalNoOfLines() throws URISyntaxException,IOException {
+        WordProcessor processor = new WordProcessor();
+        String[] options =   processor.processCommand("cwcc -w test.txt");
+        assertEquals(options[1],"-w");
+        System.out.println(options[2]);
+        System.out.println(processor.getNumberofWordsAndLines(processor.readFile(options[2]))[0]);
+        System.out.println(processor.getNumberofWordsAndLines(processor.readFile(options[2]))[1]);
+    }
+
+    @Test
+    void testTotalNoOfCharacters() throws URISyntaxException,IOException {
+        WordProcessor processor = new WordProcessor();
+        String[] options =   processor.processCommand("cwcc -w test1.txt");
+        assertEquals(options[1],"-w");
+        System.out.println(options[2]);
+        System.out.println(processor.getNumberofWordsAndLines(processor.readFile(options[2]))[1]);
+    }
+
+
+
 }
